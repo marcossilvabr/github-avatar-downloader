61145164eac6e136354efdffc19244d83de24e0d
Marcos Silva <marcosgustavobh@gmail.com>
2017-07-03 23:00:56 +0000

    added mkdir function to create a folder for avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,31 +1,47 @@
-var request = require('request');
-var fs = require('fs');
-var https = require('https');
+var input = process.argv.slice(2);
+var repoOwner = input[0];
+var repoName = input[1];
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+var fs = require("fs");
+var request = require('request');
 
 var GITHUB_USER = "marcossilvabr";
 var GITHUB_TOKEN = "c261192bad9f2a50e222fb3644d7cc39de993b33";
 
+var folderPath = "avatars/";
+
+var mkdir = function () {
+  fs.mkdir("avatars");
+}
+mkdir();
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  var urlGit = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-  var config = {
-    url: urlGit,
+  var requestOptions = {
+    url : 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
-      'User-Agent': 'GitHub Avatar Downloader - Student Project'
-    },
+      "User-Agent" : "GitHub Avatar Downloader - Student Project"
+    }
+  }
+  request.get(requestOptions, cb)
+}
+
+
+function downloadImageByUrl(url, filePath){
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+
+}
+
+getRepoContributors(repoOwner, repoName, function(err, result) {
+  var result = JSON.parse(result.body);
+  for (var i = 0; i < result.length; i++) {
+    downloadImageByUrl(result[i].avatar_url, folderPath + result[i].login + '.jpg');
   };
-
-  request(config, function(err, response, body) {
-    if (err) throw err;
-      console.log('Response Status Code', response.statusCode)
-      console.log('Response Message: ', response.statusMessage);
-      console.log('Content Type: ', response.headers['content-type']);
-  });
-};
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
 });
