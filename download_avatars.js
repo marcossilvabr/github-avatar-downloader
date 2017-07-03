466e0aeb88e771256e9dad161e527f7a275eb7cc
Marcos Silva <marcosgustavobh@gmail.com>
2017-07-03 21:36:58 +0000

    changed request to use headers

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,6 @@
 var request = require('request');
+var fs = require('fs');
+var https = require('https');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -6,10 +8,22 @@ var GITHUB_USER = "marcossilvabr";
 var GITHUB_TOKEN = "c261192bad9f2a50e222fb3644d7cc39de993b33";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var resquestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(resquestURL);
-}
+  var urlGit = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
+  var config = {
+    url: urlGit,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    },
+  };
+
+  request(config, function(err, response, body) {
+    if (err) throw err;
+      console.log('Response Status Code', response.statusCode)
+      console.log('Response Message: ', response.statusMessage);
+      console.log('Content Type: ', response.headers['content-type']);
+  });
+};
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
